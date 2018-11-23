c2a5f820ccb83267a69e26cc633ef3f3620508ec
Myth Raven <mithunan.r@gmail.com>
2018-11-23 09:22:09 -0500

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -51,9 +51,45 @@ app.get('/todos/:id', (req, res) => {
       res.send({todo});
    }).catch((e) => {
       res.status(400).send();
-   })
+   });
 
 });
+
+
+//Delete route
+
+app.delete('/todos/:id', (req,res) =>{
+   //get the id
+   var id = req.params.id;
+   //validate the id > not valid? reutrn 404. like above
+   if(!ObjectID.isValid(id)){
+      return res.status(404).send();
+   }
+
+   //remove todo by id
+   Todo.findByIdAndRemove(id).then((todo) => {
+      //success
+      if (!todo) {
+         //if no doc, send 404
+         return res.status(404).send();
+         }
+         //if doc, send doc back with 200
+         res.send({todo});
+      //error
+      }).catch((e) => {
+      //400 with empty body
+      res.status(400).send();
+   });
+
+
+
+
+
+
+
+
+});
+
 
 
 

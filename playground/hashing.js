a627dbdc326936240b3f6eb7f871c713b9718b11
Myth Raven <mithunan.r@gmail.com>
2019-01-02 11:56:45 -0500

    Hash User password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -2,17 +2,40 @@
 
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-   id : 10
-};
+var password = '727smv!';
 
-var token = jwt.sign(data, '727smv');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//    bcrypt.hash(password, salt, (err, hash) => {
+//       console.log(hash);
+//    });
+// });
 
 
-var decoded = jwt.verify(token, '727smv');
-console.log('decoded: ', decoded);
+var hashedPassword = '$2a$10$xb1K1i8W2NrIzwZSltp82e7yPvk2Z9Xr7JbiqT.aJTd1a4ApASK5q';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+   console.log(res);
+});
+
+
+
+
+
+
+
+
+// var data = {
+//    id : 10
+// };
+//
+// var token = jwt.sign(data, '727smv');
+// console.log(token);
+//
+//
+// var decoded = jwt.verify(token, '727smv');
+// console.log('decoded: ', decoded);
 
 
 
